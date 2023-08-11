2774f2b52bdbbba5e13f038754661c8469d2ec3c
dhanush <dhanushg25@gmail.com>
2023-08-11 23:51:40 +0530

    commit 1- components classified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Components/Login";
+import Homepage from "./Components/Homepage";
+import NavBar from "./Components/NavBar";
+import Dashboard from "./Components/Dashboard";
+import EditProfile from "./Components/EditProfile";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route element={<Homepage />} path="/" />
+        <Route element={<Login />} path="/login" />
+        <Route element={<Dashboard />} path="/dashboard" />
+        <Route element={<EditProfile />} path="/editprofile" />
+      </Routes>
     </div>
   );
 }
-
-export default App;
